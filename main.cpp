60a223d1e987a89a1e65cc6c0de56532fd771015
Nenu Adrian <adrian.nenu@gmail.com>
2022-01-15 12:13:46 +0000

    cmake

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+#include <iostream>
+
+
+int main( int argc, const char * argv[] )
+{
+    /*if (enet_initialize() != 0)
+    {
+        fprintf(stderr, "An error occurred while initializing ENet.\n");
+        return EXIT_FAILURE;
+    }
+
+    ENetAddress address;
+    ENetHost* server;
+
+    address.host = ENET_HOST_ANY;
+    address.port = 1234;
+    server = enet_host_create(&address ,
+        32      ,
+        2      ,
+        0     ,
+        0      );
+    if (server == NULL)
+    {
+        fprintf(stderr,
+            "An error occurred while trying to create an ENet server host.\n");
+        exit(EXIT_FAILURE);
+    }
+    while(true) {}
+     enet_host_destroy(server);*/
+
+    return 0;
+}
+
